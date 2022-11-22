512875c5174cb07a3d388a3a4b42982f13e8e042
gabrielalek <gabrielalex2002@outlook.com>
2022-11-21 23:28:39 -0300

    Commit do projeto
    Commit do projeto inteiro

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    };
+    
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+    
+    function iniciaRelogio() {
+        timer = setInterval(function() {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
+    }
+    
+    document.addEventListener('click', function(event) {
+        const element = event.target;
+        if (element.classList.contains('zerar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            relogio.innerHTML = "00:00:00";
+            segundos = 0;
+        }
+    
+        if (element.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+    
+        if (element.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
+    });
+}
+relogio();
